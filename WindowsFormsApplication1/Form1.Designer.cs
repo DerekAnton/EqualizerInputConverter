6c11a93037f27da76e078e60b2859c9948954443
DerekAnton <danton5276@gmail.com>
2015-10-25 19:22:46 -0400

    Fixed Algorithm Bug + Added default directory functionality

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -35,7 +35,7 @@
             this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(424, 350);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,7 +16,7 @@ namespace WindowsFormsApplication1
         private string[] fileLocations;
         private string parseTarget;
         private string exportValue;
-        string frequencies = "", gains = "", qualities = "", header, footer;
+        string frequencies = "", gains = "", qualities = "", vanishingCageDirectory, header, footer;
 
         public Form1()
         {
@@ -26,6 +27,10 @@ namespace WindowsFormsApplication1
             exportValue = "";
             header = "[Speakers]\r\nSpeakerId0=0\r\nSpeakerTargets0=all\r\nSpeakerName0=All\r\nSpeakerId1=1\r\nSpeakerTargets1=L\r\nSpeakerName1=Left\r\nSpeakerId2=2\r\nSpeakerTargets2=R\r\nSpeakerName2=Right\r\nSpeakerId3=3\r\nSpeakerTargets3=C\r\nSpeakerName3=Center\r\nSpeakerId4=4\r\nSpeakerTargets4=SUB\r\nSpeakerName4=Subwoofer\r\nSpeakerId5=5\r\nSpeakerTargets5=RL\r\nSpeakerName5=Left rear\r\nSpeakerId6=6\r\nSpeakerTargets6=RR\r\nSpeakerName6=Right rear\r\nSpeakerId7=7\r\nSpeakerTargets7=SL\r\nSpeakerName7=Left side\r\nSpeakerId8=8\r\nSpeakerTargets8=SR\r\nSpeakerName8=Right side\r\n[General]\r\nPreAmp=2.5\r\n";
             footer = "[Configuration]\r\nHotKey =\r\n";
+
+            // Create a default directory to use if nothing was selected.
+            vanishingCageDirectory = "C:\\Users\\Public\\VanishingCage\\";
+            Directory.CreateDirectory(vanishingCageDirectory);
         }
 
         void Form1_DragEnter(object sender, DragEventArgs e)
@@ -36,7 +41,7 @@ namespace WindowsFormsApplication1
         void Form1_DragDrop(object sender, DragEventArgs e)
         {
             fileLocations = (string[])e.Data.GetData(DataFormats.FileDrop);
-            parseTarget = System.IO.File.ReadAllText(fileLocations[0]);
+            parseTarget = File.ReadAllText(fileLocations[0]);
             parseMain(parseTarget);
         }
 
@@ -44,9 +49,9 @@ namespace WindowsFormsApplication1
         {
             target = target.Replace("\r\n", String.Empty);
             target = target.Replace("Filter Settings fileRoom EQ V5.13Dated:", String.Empty);
-            //remove the date time (might be off by 1 character if it is a 2 digit date?)
-            target = target.Substring(20, target.Length - 20);
-            target = target.Replace("PMNotes:flat-bothEqualiser: GenericAverage 1", String.Empty);
+            target = target.Substring(23, target.Length - 23);
+            target = target.Replace("MNotes", "Notes");
+            target = target.Replace("Notes:flat-bothEqualiser: GenericAverage 1", String.Empty);
             if(target[0].Equals(' '))
             {
                 target = target.Substring(1, target.Length - 1);
@@ -58,7 +63,7 @@ namespace WindowsFormsApplication1
 
                 if (target.Substring(14 + areaOfFocus, 8).Equals(" None   "))
                 {
-                    // the empty filter case. nothing to do here.
+                    // The empty filter case. nothing to do here.
                     areaOfFocus += 14;
                 }
                 else
@@ -98,13 +103,68 @@ namespace WindowsFormsApplication1
             qualities += "Quality" + (index+1) + "=" + qholder + "\r\n";
         }
 
+        string loadFromDumpFile(bool noDumpStored)
+        {
+            string directory = "";
+            DialogResult popupResult = MessageBox.Show("Please select a default folder to convert into.", "Alert", MessageBoxButtons.OK);
+
+            if (popupResult == DialogResult.OK)
+            {
+                var fD = new FolderBrowserDialog();
+
+                if (fD.ShowDialog() == DialogResult.OK)
+                {
+                    directory = fD.SelectedPath + "\\";
+                    File.WriteAllText(vanishingCageDirectory + "SavedDirectory.dump", directory );
+                    if (directory == "" || directory == null)
+                    {
+                        directory = vanishingCageDirectory;// Save to default directory 
+                    }
+                }
+                else
+                {
+                    directory = vanishingCageDirectory; // Save to default directory 
+                }
+            }
+            return directory;
+        }
+
         void createFile()
         {
-            string directory = @"C:\Users\Public\PeaceConverter\";
-            System.IO.Directory.CreateDirectory(directory);
+            // Default directory
+            string directory = "";
+            bool noDumpStored = false;
+
+            if (File.Exists(vanishingCageDirectory + "SavedDirectory.dump"))
+            {
+                directory = File.ReadAllText(vanishingCageDirectory + "SavedDirectory.dump");
+                noDumpStored = false;
+            }
+            else
+            {
+                noDumpStored = true;
+                directory = loadFromDumpFile(noDumpStored);
+            }
+
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+
+            // Create the output file in string form
             exportValue = header + frequencies + gains + qualities + footer;
-            System.IO.File.WriteAllText(@"C:\Users\Public\PeaceConverter\ConvertedFile.peace", exportValue);
-            System.Windows.Forms.MessageBox.Show("Conversion success \r\nFile saved to "+ directory);
+
+            File.WriteAllText(directory + "ConvertedFile.peace", exportValue);
+            DialogResult popupResult = MessageBox.Show("Conversion success \r\nFile saved to " + directory + "\r\n\r\n Would you like to change the default directory to convert to?", "Alert", MessageBoxButtons.YesNo);
+            if(popupResult == DialogResult.Yes)
+            {
+                directory = loadFromDumpFile(noDumpStored);
+                File.WriteAllText(directory + "ConvertedFile.peace", exportValue);
+            }
+            else
+            {/*do nothing*/}
+
         }
+
     }
 }
